5ba28b149c16fb14a9f6f51a5f6beffc0fd2950d
Juan David Moyano Gutiérrez <darhakexge@gmail.com>
2023-04-23 19:46:45 -0500

    Implementado el pipe de validación

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const configService = app.get(ConfigService);
+
+    app.setGlobalPrefix('api').useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
 
     await app.listen(configService.get<number>('app.port'));
 }
